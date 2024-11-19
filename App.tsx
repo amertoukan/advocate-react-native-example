727492f244d6a6064bd92eea8e6b9265581468b8
Amer Toukan <toukan.amer@gmail.com>
2024-11-18 18:00:10 -0700

    Commented on Steps

--- App.tsx
+++ App.tsx
@@ -8,36 +8,46 @@ const apiKey = process.env.REACT_NATIVE_API_KEY
 const accountSid = process.env.REACT_NATIVE_ACCOUNT_SID
 const JWT = process.env.REACT_NATIVE_JWT
 
-const endpoint = `https://app.referralsaasquatch.com/api/v1/${tenantAlias}/widget/account/21dd3d5674f146e0c8ffeeb43fbbc3bf292b62beaf0315b7af6bfeeef2ee0a2f/user/21dd3d5674f146e0c8ffeeb43fbbc3bf292b62beaf0315b7af6bfeeef2ee0a2f/upsert?widgetType=p%2F27048%2Fw%2FreferrerWidget&engagementMedium=EMBED`;
+//Construct the Advocate's user object
 const payload = {
-  id: '21dd3d5674f146e0c8ffeeb43fbbc3bf292b62beaf0315b7af6bfeeef2ee0a2f',
-  accountId: '21dd3d5674f146e0c8ffeeb43fbbc3bf292b62beaf0315b7af6bfeeef2ee0a2f',
+  id: '21dd3d5674f146e0c8ffeeb43fbbc3bf292b62beaf0315b7af6bfeeef2ee0a2f', //SHA1 of advocate's email - raw email can be passed if SHA1 is not available.
+  accountId: '21dd3d5674f146e0c8ffeeb43fbbc3bf292b62beaf0315b7af6bfeeef2ee0a2f', //SHA1 of advocate's email - raw email can be passed if SHA1 is not available.
+  firstName: 'John', 
+  lastName: 'Doe', 
+  email: "john.doe@example.com", 
+  locale: 'en_CA', 
+  countryCode: 'CA'
 };
+
+//impact.com/advocate's endpoint to load the widget 
+//this endpoint will require the tenant alias (provided by impact in onboarding) as well as the advocate's ID and account ID 
+const endpoint = `https://app.referralsaasquatch.com/api/v1/${tenantAlias}/widget/account/${payload.accountId}/user/${payload.id}/upsert?widgetType=p%2F27048%2Fw%2FreferrerWidget&engagementMedium=EMBED`;
+
 
 export default function App(): JSX.Element {
   const [htmlCode, setHtmlCode] = useState<string | null>(null); // State to hold the HTML response
   const [loading, setLoading] = useState<boolean>(true); // Loading state
   const [error, setError] = useState<string | null>(null); // Error state
   const auth = `Basic ${btoa(`${accountSid}:${apiKey}`)}`;
-console.log(accountSid)
-console.log(apiKey)
-  console.log(auth)
+
+
   useEffect(() => {
     const fetchHTML = async () => {
       try {
+        //Perform an API call to impact.com in order to retrieve the widget and load it for advocate
         const response = await fetch(endpoint, {
           method: 'PUT',
           headers: {
             'Content-Type': 'application/json',
-            Authorization:  auth,
+            Authorization:  auth
           },
+          //pass the advocate's user object
           body: JSON.stringify(payload),
         });
 
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
           console.log(response)
-
         }
 
         const result = await response.json();
@@ -72,11 +82,13 @@ console.log(apiKey)
   }
 
   return (
+
+    //Use a webview to load the widget content and set the baseURL as https://ssqt.co
     <WebView
       originWhitelist={['*']}
       source={{ html: htmlCode || '', baseUrl: 'https://ssqt.co' }}
       style={{ flex: 1 }}
-      contentMode='mobile'// Example conditional
+      contentMode='mobile'
       javaScriptEnabled={true}
     />
   );
